39d9ec1a07014283eb3485a8287561e3ab808e32
Julio Cesar <145167974+Juli-oCosta@users.noreply.github.com>
2025-09-23 16:05:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 logo = """
  _____________________
 |  _________________  |
-| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |       2 + 2 = 4 | |  .----------------.  .----------------.  .----------------.  .----------------. 
 | |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
 |  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
 | | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
@@ -17,3 +17,39 @@ logo = """
 """
 
 print(logo)
+
+operations = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide
+}
+
+def add(num1, num2):
+  return num1 + num2
+
+def subtract(num1, num2):
+  return num1 - num2
+
+def multiply(num1, num2):
+  return num1 * num2
+
+def divide(num1, num2):
+  if num2 == 0:
+    return "Cannot divide by zero."
+  else:
+    return num1 / num2
+
+first_number = int(input("Type in the first number: "))
+second_number = int(input("Type in the second number: "))
+operator = input("Select the operator: '+', '-', '*' or '/': ")
+
+if operator == "+":
+  add(num1 = first_number, num2 = second_number)
+elif operator == "-":
+  subtract(num1 = first_number, num2 = second_number)
+elif operator == "*":
+  multiply(num1 = first_number, num2 = second_number)
+else:
+  divide(num1 = first_number, num2 = second_number)
+
